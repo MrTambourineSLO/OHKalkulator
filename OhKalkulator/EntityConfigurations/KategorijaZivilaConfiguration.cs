7998c19fbe221b3853cca7c3ee4b9f60b3ef8c88
Saso Miocinovic <saso.miocinovic@gmail.com>
2016-08-18 20:20:53 +0200

    Somehow I forgot those two files in previous commit.

---
+++ OhKalkulator/EntityConfigurations/KategorijaZivilaConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity.ModelConfiguration;
+using OhKalkulator.Models;
+
+namespace OhKalkulator.EntityConfigurations
+{
+    public class KategorijaZivilaConfiguration : EntityTypeConfiguration<KategorijaZivila>
+    {
+        /*===   Overridanje konvencij za tabelo KategorijaZivila  ===*/
+
+        public KategorijaZivilaConfiguration()
+        {
+            ToTable("KategorijeZivil")
+           .Property(p => p.Ime)
+           .HasColumnType("nvarchar")
+           .HasMaxLength(255)
+           .IsRequired();
+            /*
+              ===   Overridanje konvencij za relacijo KategorijaZivila(1) -> (N)Zivila  ===
+           */
+
+            HasMany(p => p.Zivila)
+                .WithRequired(p => p.KategorijaZivila)
+                .HasForeignKey(f => f.KategorijaZivilaId);
+        }
+    }
+}
---
+++ OhKalkulator/EntityConfigurations/ZiviloConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity.ModelConfiguration;
+using OhKalkulator.Models;
+
+
+namespace OhKalkulator.EntityConfigurations
+{
+    public class ZiviloConfiguration : EntityTypeConfiguration<Zivilo>
+    {
+        /*===   Overridanje konvencij za tabelo Zivila  ===*/
+        
+        public ZiviloConfiguration()
+        {
+                ToTable("Zivila")
+                .Property(p => p.Ime)
+                .IsRequired()
+                .HasColumnType("nvarchar")
+                .HasMaxLength(255);
+
+                 Property(p => p.OhKoeficient).IsRequired();
+            
+                 Property(p => p.SifraGorenje)
+                .IsOptional();
+        }
+
+    }
+}
