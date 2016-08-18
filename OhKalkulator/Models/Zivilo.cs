1a6c059809ec6bddcf6b2bcb4eb46b624cfd4484
Saso Miocinovic <saso.miocinovic@gmail.com>
2016-08-18 13:54:55 +0200

    Initial migration model and created Zivilo model.

---
+++ OhKalkulator/Models/Zivilo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OhKalkulator.Models
+{
+    public class Zivilo
+    {
+        public int Id { get; set; }
+        public int SifraGorenje { get; set; }
+        public string Ime { get; set; }
+        public int TezaVGramih { get; set; }
+        public decimal OhKoeficient { get; set; }
+        //public DomacaMera DomacaMera { get; set; }
+        //public KategorijaZivila KategorijaZivila { get; set; }
+    }
+}
