f2dd35b068824b5a687d392148a433d42dde7cb3
Saso Miocinovic <saso.miocinovic@gmail.com>
2016-11-06 23:34:46 +0100

    Added excluded files.

---
+++ OhKalkulator/EntityConfigurations/ApplicationUserConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity.ModelConfiguration;
+using OhKalkulator.Models;
+
+namespace OhKalkulator.EntityConfigurations
+{
+    public class ApplicationUserConfiguration :EntityTypeConfiguration<ApplicationUser>
+    {
+        public ApplicationUserConfiguration()
+        {
+            HasRequired(p => p.DnevnikPrehranjevanja).WithRequiredPrincipal(p => p.Uporabnik);
+        }
+    }
+}
---
+++ OhKalkulator/EntityConfigurations/DnevnikPrehranjevanjaConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity.ModelConfiguration;
+using OhKalkulator.Models;
+
+namespace OhKalkulator.EntityConfigurations
+{
+    public class DnevnikPrehranjevanjaConfiguration : EntityTypeConfiguration<DnevnikPrehranjevanja>
+    {
+        public DnevnikPrehranjevanjaConfiguration()
+        {
+            ToTable("DnevnikiPrehranjevanja");
+            Property(p => p.Datum).HasColumnType("datetime2").IsRequired();
+            Property(p => p.TipObroka).IsRequired();
+            HasRequired(p => p.Obrok).WithOptional(p => p.DnevnikPrehranjevanja);
+
+            
+        }
+    }
+}
